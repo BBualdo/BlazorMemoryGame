9434747c005afbb94d879010e0084cbcf6edd2ec
Sebastian <mrseb10@gmail.com>
2024-06-26 09:47:16 +0200

    Username Picker component

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,19 +1,21 @@
 using UI.Components;
+using UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+  .AddInteractiveServerComponents();
+builder.Services.AddSingleton<GamesService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error", true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +24,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+  .AddInteractiveServerRenderMode();
 
 app.Run();
---
+++ UI/Services/GamesService.cs
@@ -0,0 +1,20 @@
+﻿using Data.Enums;
+using Data.Models;
+
+namespace UI.Services;
+
+public class GamesService
+{
+  public GameStep CurrentStep = GameStep.Username;
+  public Game Game = new();
+
+  public void SetUsername(string username)
+  {
+    Game.Username = username;
+  }
+
+  public void SetDifficulty(DifficultyLevels difficulty)
+  {
+    Game.Difficulty = difficulty;
+  }
+}
